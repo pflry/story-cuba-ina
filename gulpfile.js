a3e3f6bff7078e921b612b38dab7083cb9f10d2e
Paul Fleury <loporifle@gmail.com>
2019-05-07 11:00:04 +0200

    yarn + gulpv4 + nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,302 +1,238 @@
-//-- Dependencies
-const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    clean = require('gulp-clean'),
-    autoprefixer = require('gulp-autoprefixer'),
-    rename = require('gulp-rename'),
-    changed = require('gulp-changed'),
-    htmlReplace = require('gulp-html-replace'),
-    imagemin = require('gulp-imagemin'),
-    imageminMozjpeg = require('imagemin-mozjpeg'),
-    webp = require('gulp-webp'),
-    sequence = require('gulp-sequence'),
-    mustache = require("gulp-mustache"),
-    replace = require('gulp-replace'),
-    shell = require('gulp-shell'),
-    connect = require('gulp-connect-php'),
-    fs = require('fs'),
-    browserSync = require('browser-sync').create();
+const { dest, lastRun, parallel, series, src, watch} = require('gulp');
 
+const autoprefixer = require('gulp-autoprefixer');
+const browsersync = require('browser-sync').create();
+const data = require('gulp-data');
+const del = require("del");
+const imagemin = require('gulp-imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const nunjucksRender = require('gulp-nunjucks-render');
+const rename = require('gulp-rename');
+const replace = require('gulp-replace');
+const sass = require('gulp-sass');
+const webp = require('gulp-webp');
+const yaml = require('gulp-yaml');
 
 //-- Constantes
-const flag = 'story-cuba';
-const storyTitle = 'La Révolution cubaine';
-const absoluteURL = 'https://ina.pflry.eu/' + flag + '/';
+const folder = 'story-brexit';
+const absoluteUrl = 'https://ina.pflry.eu/' + folder + '/';
 
 //-- Path
 const path = {
-    medias : './medias-src/',
-    src :   './src/',
-    dev :   './build/' + flag + '/',
-    prod : './export/' + flag + '/',
-    icons : 'assets/icons/',
-    images : 'assets/images/',
-    videos : 'assets/videos/',
-    audios : 'assets/audios/',
-    jsons : './src/json/',
-    partials : './src/partials/',
-    templates : './src/templates/',
-    scss : './src/sass/'
+  src : 'src/',
+  dev : 'dev/' + folder + '/',
+  prod: 'export/' + folder + '/'
 };
-
-
-
-//-- MEDIAS
-//-------------
-// la tâche image doit toujours être passée en premier
-
-//-- Images
-
-// optimize jpg
-gulp.task('images:jpg', () => {
-    gulp.src(path.medias + 'images/*.jpg')
-        .pipe(changed(path.medias + 'images/' + path.src + path.images))
-        .pipe(imagemin([imageminMozjpeg({
-            quality: 60
-        })]))
-        .pipe(gulp.dest(path.src + path.images));
-});
-
-
-//-- Audios
-
-// normalize + convert to mp3/ogg + create file subtitles.vtt
-gulp.task('audios', shell.task(['FOR %A IN ("medias-src/audios/*.wav") DO bash create-audios.sh medias-src/audios/%A src/assets/audios/%~NA']));
-
-// copy posters
-gulp.task('posters:copy', shell.task(['FOR %A IN ("medias-src/audios/*.wav") DO COPY /B /Y %CD%\\src\\assets\\images\\%~NA.jpg %CD%\\src\\assets\\audios\\%~NA\\poster.jpg']));
-
-// audio together
-gulp.task('audios:convert', sequence('audios', 'posters:copy'));
-
-
-//-- Videos
-
-// normalize + convert to mp4/webm
-// + create poster + create file subtitles.vtt
-gulp.task('videos:convert', shell.task(['FOR %A IN ("medias-src/videos/*.avi") DO bash create-videos.sh medias-src/videos/%A src/assets/videos/%~NA']));
-
 
 
 //-- DEV
 //-------------
 
-//-- Create mustache partials from jsons task
-gulp.task('partials:clean', () => {
-    return gulp.src(path.partials, {
-            read: false
-        })
-        .pipe(clean());
-});
+//-- SCSS
+function css() {
+  return src(path.src + 'sass/styles.scss', { since: lastRun(css) })
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(rename('amp-styles.njk'))
+    .pipe(dest(path.src + 'templates/partials/'));
+}
+exports.css = css;
 
-function createPartials(dir) {
-    fs.readdir(dir, (err, files) => {
-        if (err) {
-            return console.error(err);
-        }
-        files.forEach(function (file) {
-            file = file.split('.').slice(0, -1).join('.');
-            gulp.src(path.templates + '*.mustache')
-                .pipe(mustache(path.jsons + file + '.json', {}, {}))
-                .pipe(rename(file + '.mustache'))
-                .pipe(gulp.dest(path.partials));
-        });
-    });
-};
+//-- YAML
+function yml_content() {
+  return src(path.src + 'content.yml', { since: lastRun(yml_content) })
+    .pipe(yaml({ space: 2 }))
+    .pipe(dest(path.src + 'json/'));
+}
 
-gulp.task('partials', () => {
-    createPartials(path.jsons);
-});
+function yml_bookend() {
+  return src(path.src + 'bookend.yml', { since: lastRun(yml_bookend) })
+    .pipe(yaml({ space: 2 }))
+    .pipe(dest(path.dev));
+}
 
-//-- Generate index.html in build task
-// Add css
-// Add calls to partials
-let buffer = new Buffer('');
+const yml = parallel(
+  yml_content,
+  yml_bookend  
+);
+exports.yml = yml;
 
-function generateIndex(dir) {
-    fs.readdir(dir, function (err, files) {
-        if (err) {
-            return console.error(err);
-        }
-        files.forEach(function (file) {
-            file = file.split('.').slice(0, -1).join('.');
-            buffer += '{{> ../../partials/' + file + ' }}\n\n\t\t\t'
-        });
-        // return console.log(buffer);
-        gulp.src(path.templates + 'page/tpl-index.mustache')
-            .pipe(htmlReplace({
-                cssInline: {
-                    src: gulp.src(path.scss + 'styles.scss')
-                        .pipe(sass({
-                            outputStyle: 'compressed'
-                        }).on('error', sass.logError))
-                        .pipe(autoprefixer({
-                            browsers: ['last 2 versions']
-                        })),
-                    tpl: '<style amp-custom>%s</style>'
-                },
-                blocks: {
-                    src: buffer,
-                    tpl: '%s'
-                }
-            }))
-            .pipe(mustache({
-                title: storyTitle,
-                publisher: "Institut national de l&#39;audiovisuel"
-            }))
-            .pipe(rename('index.html'))
-            .pipe(gulp.dest(path.dev));
-    });
-};
+//-- Nunjucks
+function njk() {
+  return src(path.src + 'templates/pages/*.njk', { since: lastRun(njk) })
+    .pipe(data(function () {
+      return require('./' + path.src + 'json/content.json')
+    }))
+    .pipe(nunjucksRender({
+      path: [path.src + 'templates']
+    }))
+    .pipe(dest(path.dev));
+}
+exports.njk = njk;
 
-gulp.task('index', () => {
-    generateIndex(path.partials);
-});
+//-- Webp
+function image_webp() {
+  return src(path.src + 'assets/images/*.jpg', { since: lastRun(image_webp) })
+    .pipe(webp({
+      quality: 60,
+      preset: 'photo',
+      method: 6
+    }))
+    .pipe(dest(path.dev + 'assets/images/'))
+}
 
-//-- copy function
-function kopy(valsrc, valdest) {
-    gulp.src(valsrc)
-        .pipe(changed(valdest))
-        .pipe(gulp.dest(valdest))
-};
+//-- Jpeg
+function image_jpg() {
+  return src(path.src + 'assets/images/*.jpg', { since: lastRun(image_jpg) })  
+    .pipe(imagemin([imageminMozjpeg({
+      quality: 60
+    })]))
+    .pipe(dest(path.dev + 'assets/images/'));
+}
 
-//-- bookend:copy task
-gulp.task('bookend:copy', () => {
-    kopy(path.src + 'bookend.json', path.dev);
-});
+function poster_jpg() {
+  return src(path.src + 'assets/poster/*.jpg', { since: lastRun(poster_jpg) })
+    .pipe(imagemin([imageminMozjpeg({
+      quality: 60
+    })]))
+    .pipe(dest(path.dev + 'assets/poster/'))
+}
 
-//-- favicon:copy task
-gulp.task('favicon:copy', () => {
-    kopy('favicon.ico', path.dev);
-});
+//-- Copy
+function file_copy() {
+  return src(['favicon.ico'], { since: lastRun(file_copy) })
+    .pipe(dest(path.dev))
+}
 
-//-- icons:copy task
-gulp.task('icons:copy', () => {
-    kopy(path.src + path.icons + '*.*', path.dev + path.icons);
-});
+function icon_copy() {
+  return src(path.src + 'assets/icons/*.*', { since: lastRun(icon_copy) })
+    .pipe(dest(path.dev + 'assets/icons/'))
+}
 
-//-- videos:copy task
-gulp.task('videos:copy', () => {
-    kopy(path.src + path.videos + '/**/*.*', path.dev + path.videos);
-});
+function video_copy() {
+  return src(path.src + 'assets/videos/**/*.*', { since: lastRun(video_copy) })
+    .pipe(dest(path.dev + 'assets/videos/'))
+}
 
-//-- audios:copy task
-gulp.task('audios:copy', () => {
-    kopy(path.src + path.audios + '/**/*.*', path.dev + path.audios);
-});
+function logo_copy() {
+  return src(path.src + 'assets/logo/*.*', { since: lastRun(logo_copy) })
+    .pipe(dest(path.dev + 'assets/logo/'))
+}
 
-//-- images:copy
+//-- BrowserSync
+function reload(done) {
+  browsersync.reload();
+  done();
+}
 
-// contert jpg to webp + copy
-gulp.task('webp:copy', () => {
-    gulp.src(path.src + path.images + '*.jpg')
-        .pipe(webp({
-            quality: 60,
-            preset: 'photo',
-            method: 6
-        }))
-        .pipe(gulp.dest(path.dev + path.images));
-})
-
-gulp.task('jpg:copy', () => {
-    kopy(path.src + path.images + '*.jpg', path.dev + path.images);
-});
-
-// together images
-gulp.task('images:copy', sequence('jpg:copy', 'webp:copy'));
+function serve(done) {
+  browsersync.init({
+    server: {
+      baseDir: path.dev
+    }
+  });
+  watch(path.src + '*.yml', series(yml, njk));
+  watch(path.src + 'sass/**/*.scss', series(css, njk));
+  watch(path.src + 'templates/**/*.njk', series(njk));
+  done();
+}
 
 
-//-- Clean task dev (delete build folder)
-gulp.task('dev:clean', function () {
-    return gulp.src('./build/', {
-            read: false
-        })
-        .pipe(clean());
-});
-
-//-- ConnectPHP + BrowserSync task
-gulp.task('serve', () => {
-    connect.server({}, function () {
-        browserSync.init({
-            proxy: 'http://127.0.0.1:8000/build/' + flag + '/' + 'index.html'
-        });
-    });
-
-    gulp.watch(path.dev + 'index.html').on('change', browserSync.reload);
-});
-
-gulp.task('serve:static', () => {
-    browserSync.init({
-        server: {
-            baseDir: path.dev
-        }
-    });
-
-    gulp.watch(path.dev + 'index.html').on('change', browserSync.reload);
-});
-
-
-//-- PRODUCTION
+//-- PROD
 //-------------
 
-//-- URL remplace relative par absolute URL's
-gulp.task('urls', function () {
-    gulp.src([path.dev + 'index.html'])
-        .pipe(replace(/assets/g, '' + absoluteURL + 'assets'))
-        .pipe(replace('src="bookend.json"', 'src="' + absoluteURL + 'bookend.json"'))
-        .pipe(gulp.dest(path.prod));
-});
+//-- URLs
+function  prod_url() {
+  return src(path.dev + '/index.html', { since: lastRun(prod_url) })
+    .pipe(replace(/assets/g, '' + absoluteUrl + 'assets'))
+    .pipe(replace('src="bookend.json"', 'src="' + absoluteUrl + 'bookend.json"'))
+    .pipe(dest(path.prod));
+}
 
-//-- Copy assets prod
-gulp.task('prod:copy', () => {
-    kopy(path.dev + 'bookend.json', path.prod);
-    kopy(path.dev + 'favicon.ico', path.prod);
-    kopy(path.dev + 'sw.js', path.prod);
-    kopy(path.dev + 'assets/**/*.*', path.prod + 'assets/');
-});
+//-- Copy
+function prod_asset() {
+  return src(path.dev + 'assets/**/*.*', { since: lastRun(prod_asset) })
+    .pipe(dest(path.prod + '/assets/'))
+}
 
-//-- Clean folder (delete export folder)
-gulp.task('prod:clean', function () {
-    return gulp.src('./export/', {
-        read: false
-    })
-        .pipe(clean());
-});
-
-
+function prod_file() {
+  return src([
+    path.dev + 'bookend.json',
+    path.dev + 'favicon.ico'
+  ], { since: lastRun(prod_file) })
+  .pipe(dest(path.prod))
+}
 
 /**
- * CLEAN TASK
+ * DEV TASKS
  */
-gulp.task('clean', ['dev:clean', 'prod:clean']);
+
+//-- Copy task
+const copy = parallel(
+  file_copy,
+  icon_copy,
+  video_copy,
+  logo_copy,
+  series(
+    image_jpg,
+    image_webp,
+    poster_jpg
+  )
+);
+exports.copy = copy;
+
+//-- Compile, generate and copy task
+exports.dev = series(
+  css,
+  yml,
+  njk,
+  copy,
+  reload
+);
+
+//-- Compile and generate task
+exports.devlight = series(
+  css,
+  yml,
+  njk,
+  reload
+);
+
+//-- Default
+exports.default = serve;
 
 /**
- * COPY TASK
+ * PROD TASKS
  */
-gulp.task('dev:copy', ['images:copy', 'icons:copy', 'videos:copy', 'audios:copy', 'bookend:copy', 'favicon:copy']);
+exports.prod = series(
+  prod_url,
+  prod_asset,
+  prod_file
+);
 
 /**
- * DEV TASK
+ * CLEAN TASKS
  */
-gulp.task('dev', function (callback) {
-    sequence('dev:clean', 'dev:copy', 'partials', 'index')(callback)
-});
 
-/**
- * PROD TASK
- */
-gulp.task('prod', function (callback) {
-    sequence('prod:clean', 'prod:copy', 'urls')(callback)
-});
+//-- Dev
+function clean_dev() {
+  return del([path.dev]);
+}
+exports.clean_dev = clean_dev;
 
-/**
- * LIGHT TASK
- */
-gulp.task('light', function (callback) {
-    sequence('partials', 'index')(callback)
-});
+//-- Prod
+function clean_prod() {
+  return del([path.prod]);
+}
+exports.clean_prod = clean_prod;
 
-/**
- * DEFAULT
- */
-gulp.task('default', ['serve']);
+//-- All
+exports.clean = parallel(
+  clean_dev,
+  clean_prod
+);
+
